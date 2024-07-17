ba0d7b5ef782e3d8e49d048c16cbea44403b2de1
DigitalWNZ <ext_wangez_google_com@allen-working-vm.us-central1-a.c.agolis-allen-first.internal>
2024-07-17 15:28:26 +0000

    update bootstrap.sh

--- tpcds_dataproc/tpcds_bootstrap.sh
+++ tpcds_dataproc/tpcds_bootstrap.sh
@@ -2,7 +2,7 @@
 apt install wget tmux -y
 cd /opt
 git clone https://github.com/DigitalWNZ/oss_util.git
-cd gcp-samples/tpcds_dataproc
+cd oss_util/tpcds_dataproc
 tar -zxvf tpcds-kit.tar.gz
 ROOT_DIR=$(/usr/share/google/get_metadata_value attributes/ROOT_DIR)
 sed -i "s#<ROOT_DIR>#${ROOT_DIR}#g" datagen.scala
